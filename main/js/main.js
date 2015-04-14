82731d389912d54338b23136434108d68c109890
Tran Dinh Thien Phuc <dinht@Trans-Mac-Pro.local>
2015-04-14 23:28:28 +0300

    Finish basic sidebar layout

---
+++ main/js/main.js
@@ -0,0 +1,20 @@
+$(window).load(function() {
+	// glDatePicker scripts
+	$('#mydate').glDatePicker(
+		{
+			cssName: 'flatwhite',
+			containerWidth: '250px',
+			showAlways: true,
+		    allowMonthSelect: false,
+		    allowYearSelect: false,
+		    prevArrow: '\u25c4',
+		    nextArrow: '\u25ba',
+		    selectedDate: new Date()
+		}
+	);
+})
+
+
+$(document).ready(function() {
+
+})
